0ba8b34ef868c9aaaf40423abf773c922217ede4
vividsystem <notrealbutnotobot@gmail.com>
2023-05-19 22:12:03 +0200

    feat: added floating diagnostics

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,5 +1,36 @@
 local M =  {}
 
+local config = {
+		virtual_text =  {
+			severity = {
+				min = vim.diagnostic.severity.ERROR
+			}
+		},
+		float = {
+			focus = false,
+			focusable = true,
+			closable = true,
+			source = 'always',
+			style = 'minimal',
+			border = 'double',
+			header = "",
+			prefix = ""
+		}
+}
+
+function update_diagnostics()
+	local diagnostics = vim.diagnostic.get(0)
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	for _, diagnostic in ipairs(diagnostics) do
+		vim.diagnostic.open_float(
+			bufnr,
+			diagnostic,
+			config.float
+		)
+	end
+end
+
 function M.setup(servers)
 	local lspconfig = require("lspconfig")
 	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -9,9 +40,11 @@ function M.setup(servers)
 --		capabilities = capabilities
 --	}
 
+	vim.diagnostic.config(config)
+
   for server, opts in pairs(servers) do
     lspconfig[server].setup(vim.tbl_deep_extend("force", {
-    	on_attach = function ()
+    	on_attach = function (_, bufnr)
 				require('lsp_signature').on_attach({
 						bind = true,
 						hint_enable = false,
@@ -23,6 +56,9 @@ function M.setup(servers)
 			capabilities = capabilities,
     }, opts or {}))
   end
+
+	vim.cmd [[ autocmd CursorMoved * lua update_diagnostics() ]]
+
 end
 
 return M
